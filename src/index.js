c7394f44005988f9eb30290c8b05b13e96ea6f26
Filipe Tenedini <105571583+FilipeTenedini@users.noreply.github.com>
2023-05-12 16:24:03 -0300

    (chore) edit index.js

--- src/index.js
+++ src/index.js
@@ -5,12 +5,9 @@ import ThemeProv from './contexts/ThemeContext/ThemeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  //   <ThemeProv>
-  //     <App />
-  //   </ThemeProv>
-  // </React.StrictMode>,
-  <ThemeProv>
-    <App />
-  </ThemeProv>,
+  <React.StrictMode>
+    <ThemeProv>
+      <App />
+    </ThemeProv>
+  </React.StrictMode>,
 );
